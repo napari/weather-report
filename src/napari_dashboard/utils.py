3c52d4e290c7322c48aedcd8dada27596862c746
Grzegorz Bokota <bokota+github@gmail.com>
2025-10-20 16:21:37 +0200

    Increase sleep on server 500 error

--- src/napari_dashboard/utils.py
+++ src/napari_dashboard/utils.py
@@ -5,9 +5,9 @@ import requests
 
 def requests_get(url: str, depth: int = 10):
     """this is util function to workaround a problem with status 500 in vercell"""
-    for _ in range(depth):
+    for i in range(1, depth + 1):
         res = requests.get(url)
         if res.status_code != 500:
             return res
-        time.sleep(1)  # wait
+        time.sleep(i)  # wait before retrying, longer each time
     raise RuntimeError(f"Failed to get data from the server for {url}")
